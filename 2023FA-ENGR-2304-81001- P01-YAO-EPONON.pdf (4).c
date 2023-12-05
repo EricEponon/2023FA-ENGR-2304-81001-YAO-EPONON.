bca32cb26c6e8ee97f9f0404982ac38b9a661f41
EricEponon <151000221+EricEponon@users.noreply.github.com>
2023-12-05 08:52:58 -0600

    Add files via upload

---
+++ 2023FA-ENGR-2304-81001- P01-YAO-EPONON.pdf (4).c
@@ -0,0 +1,10 @@
+/* 2023FA-ENGR-2304-81001-P01-YAo-Eponon*/
+#include<stdio.h>
+
+int main(){
+    printf(" Hello, World ! ");
+    printf("welcome to ENGR 2304 - Programming for Engineers at Dallas College");
+    return 0;
+}
+    
+    
